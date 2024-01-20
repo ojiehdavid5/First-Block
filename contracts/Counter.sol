0849340ae2ad849c49ef0fc535305aac06dde7a1
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-20 22:18:49 +0100

    nothing new

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -8,17 +8,11 @@ pragma solidity ^0.8.0;
 
 contract Counter{
     //code for the smart contract goes here 
-    uint  count;/// only positive integer
+    uint   public count =0;/// only positive integer
 
-    constructor()public{
-        count = 0;
-    }
-    function getCount() public view returns(uint){
-        return count;
-    }
 
     function incrementCount() public  returns(uint){
-        count=count +1;
+        count++;
         return count;
     }
 }
---
+++ contracts/MyContract.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+
+
+contract MyContract{
+//State variable
+uint MyUint =1
+
+int256 myInt256 public=1;
+uint256 MyUint256 public=1;
+uint8 MyUint8 public=1;
+
+string public myString="my first string";
+//Local Variable
+
+function getValue() public public pure returns(uint){
+    uint value =0;
+    return value;
+}
+
+}
