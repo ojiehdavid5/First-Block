90a7bd96590a011eae4ed57198242a4e8730bd27
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-22 12:37:12 +0100

    nothing new

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,32 +1,71 @@
-//SPDX-License-Identifier:MIT
+
+//SPDX-License Identfier:MIT
 
 pragma solidity ^0.8.0;
 
 
 
-contract MyContract{
-//State variable
-uint MyUint =1;
+contract MyContract {
 
-int256 public myInt=1;
-uint256  public MyUint =1;
-uint8  public MyUint =1;
+//Arrays
 
-string public myString="my first string";
-byte32 public myByte32="Helloworld";
-address public myAddress=bgth0987654321234567890098765434567;
-struct  myStruct{
-uint256 myUint256
-string myString;
+uint[] public uintArray=[1,2,3];
 
+string[] public stringArray=["Apple","Banana","mango"];
+string [] public values;
+
+
+function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+
+
+function valueCount( ) public view returns(uint){
+
+return values.length;
 }
 
-MyStruct public myStruct=MyStruct(1,'Helloworld');
-//Local Variable
 
-function getValue() public public pure returns(uint){
-    uint value =0;
-    return value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // State variables
+    // uint256 public MyUint = 1;
+    // int256 public myInt = 1;
+    // uint8 public myUint8 = 1;
+
+    // string public myString = "my first string";
+    // bytes32 public myByte32 = bytes32("Helloworld");
+    // address public myAddress = address(0x1234567890123456789012345678901234567890);
+
+    // struct MyStruct {
+    //     uint256 myUint256;
+    //     string myString;
+    // }
+
+    // MyStruct public myStruct = MyStruct(1, "Helloworld");
+
+    // // Local Variable
+    // function getValue() public pure returns (uint256) {
+    //     uint256 value = 0;
+    //     return value;
+    // }
 }
 
-}
+
+
+
