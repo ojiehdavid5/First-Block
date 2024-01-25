350fa865e2c1a6c7b1ecf1ca4216644f33eb602c
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-25 14:46:18 +0100

    test

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -23,12 +23,30 @@ contract MyContract{
 
 
 
-uint[] public  numbers  =[1,2,3,4];
+uint[] public  number =[1,2,3,4,5,6,7,8,9,10];
+
+
+function countEvenNumbers () public view returns(uint){
+uint count=0;
+
+for(uint i=0; i<number.length; i++){
+
+    if(isNumberEven(number[i])){
+        count++;
+    }
+}
+
+
+
+ return count;
+
+
+}
 
 
 function isNumberEven(uint _number)  public returns(bool) {
 
-    if( _numbers  % 2 == 0){
+    if( _number  % 2 == 0){
         return true;
     }else{
         return false;
