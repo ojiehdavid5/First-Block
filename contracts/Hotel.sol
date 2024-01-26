0dbc0570c58fe83e9f998f88fe5dc71978b40760
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-26 19:50:21 +0100

    payment using ether

--- contracts/Hotel.sol
+++ contracts/Hotel.sol
@@ -15,7 +15,13 @@ contract HotelRoom{
 
     //vacant 
     //occupy
-    enum Statuses{Vacant,occupied};
+    enum Statuses{Vacant,Occupied};
+
+
+
+    event Occuppy(address _occupant,uint _value);
+
+
     Statuses currentStatuses;
 
 
@@ -25,10 +31,33 @@ address payable public owner;
 constructor(){
     owner=msg.sender;
 }
-    function book payable (){
+
+
+modifier  onlyWhileVacant{
+        require(currentStatuses==Statuses.vacant,'currently occupied');
+
+}
+
+
+modifier costs(uint _amount){
+
+require(msg.value>= _amount ,'Not enough ether provided');
+
+
+}
+    function book() payable onlyWhileVacant costs(2 ether) {
+
+        //check price
+
+        //check status
+
         currentStatuses=Statuses.Occupied
 
         owner.transfer(msg.value);
+       (bool sent,bytes memory data)= owner.call{value:msg.value}("");
+       require(true);
+
+        emit Occupy(msg.sender, msg.value);
 
 
     }
