495a3a22055359df9882a9e18c76c190d6cb0064
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-27 19:42:50 +0100

    making a secret contract

--- contracts/Hotel.sol
+++ contracts/Hotel.sol
@@ -4,67 +4,127 @@
 pragma solidity ^0.8.0;
 
 
-contract HotelRoom{
-    //////////Ether payment
-    //Modifiers
-    //visibility
-    //Events
-    //Enums
+contract Ownable{
 
 
+        address owner;
 
-    //vacant 
-    //occupy
-    enum Statuses{Vacant,Occupied}
-
-
-
-event Occupy(address _occupant,uint _value);
-
-    Statuses public currentStatuses;
-
-
-
-address payable public owner;
-
-constructor(){
-    owner=payable(msg.sender);
-}
-
-
-modifier  onlyWhileVacant{
-        require(currentStatuses==Statuses.Vacant,'currently occupied');
+        modifier onlyOwner(){
+        require(msg.sender==owner,'must be owner');
         _;
-
-}
-
-
-modifier costs(uint _amount){
-
-require(msg.value>= _amount ,'Not enough ether provided');
-_;
+    }
 
 
-}
-    function book() public  payable onlyWhileVacant costs(2 ether) {
-
-        //check price
-
-        //check status
-
-        currentStatuses=Statuses.Occupied;
-
-        owner.transfer(msg.value);
-
-(bool success, ) = owner.call{value: msg.value}("");
-
-         require(success,'Transfer failed');
+        constructor(string memory _secret)public{
 
 
-
-        emit Occupy(msg.sender, msg.value);
+        owner=msg.sender;
 
 
     }
 
+
 }
+
+
+
+contract MyContract is Ownable{
+
+
+    string secret;
+
+
+
+
+    constructor(string memory _secret)public{
+
+
+        secret =_secret;
+        owner=msg.sender;
+
+
+    }
+
+    function getSecret()public view returns(string memory){
+        return secret;
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// contract HotelRoom{
+//     //////////Ether payment
+//     //Modifiers
+//     //visibility
+//     //Events
+//     //Enums
+
+
+
+//     //vacant 
+//     //occupy
+//     enum Statuses{Vacant,Occupied}
+
+
+
+// event Occupy(address _occupant,uint _value);
+
+//     Statuses public currentStatuses;
+
+
+
+// address payable public owner;
+
+// constructor(){
+//     owner=payable(msg.sender);
+// }
+
+
+// modifier  onlyWhileVacant{
+//         require(currentStatuses==Statuses.Vacant,'currently occupied');
+//         _;
+
+// }
+
+
+// modifier costs(uint _amount){
+
+// require(msg.value>= _amount ,'Not enough ether provided');
+// _;
+
+
+// }
+//     function book() public  payable onlyWhileVacant costs(2 ether) {
+
+//         //check price
+
+//         //check status
+
+//         currentStatuses=Statuses.Occupied;
+
+//         owner.transfer(msg.value);
+
+// (bool success, ) = owner.call{value: msg.value}("");
+
+//          require(success,'Transfer failed');
+
+
+
+//         emit Occupy(msg.sender, msg.value);
+
+
+//     }
+
+// }
